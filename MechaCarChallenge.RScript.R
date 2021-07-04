622b474aaeccfce5faad1eb3ae82d47b9c8edb34
Matthew R. Serventi <Matt@Matthews-MacBook-Pro.local>
2021-07-03 20:59:08 -0700

    Adding Deliverable 2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -11,3 +11,18 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Summary of linear regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mechacar_mpg_data))
+
+
+# Deliverable 2
+
+# Import the suspension coil data
+suspension_data <- read.csv("Suspension_Coil.csv", stringsAsFactors = F)
+
+# Create a total_summary DF using the summarize() function
+total_summary <- suspension_data %>% summarize(Mean=mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# Create a lot_summary DF
+lot_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# Deliverable 3
+
